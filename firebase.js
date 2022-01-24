07d20eea5288bf064a2585513861e4aa1ab5f498
A.G.Sudani <36556422+ahmedsmrt@users.noreply.github.com>
2022-01-24 05:44:17 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default  firebaseConfig = {
+  apiKey: "AIzaSyDX633NoJX6oPdKnEue-53Uxn5EmkvAWJ4",
+  authDomain: "emotion-wheel-ba5e1.firebaseapp.com",
+  projectId: "emotion-wheel-ba5e1",
+  storageBucket: "emotion-wheel-ba5e1.appspot.com",
+  messagingSenderId: "66345696406",
+  appId: "1:66345696406:web:fffe49a6f3c0fe91a1d6b7",
+  measurementId: "G-TWMLJ4FQ75",
+};
+
+// Initialize Firebase
+
+export default app = initializeApp(firebaseConfig);
